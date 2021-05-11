44c1c6d46dd64de73285e660bd6b3a1ab14bceeb
wing <denkondrat13+1@gmail.com>
2021-05-11 21:47:12 +0300

    Lesson Log 11.05

---
+++ constants/base.py
@@ -0,0 +1,4 @@
+"""Store base locators and useful links"""
+from constants.start_page import START_PAGE_URL
+
+PROFILE_PAGE_LINK = f"{START_PAGE_URL}" + "profile/{username}"
---
+++ constants/profile_page.py
@@ -0,0 +1,8 @@
+"""Storre useful constants and links for Profile Page"""
+
+PROFILE_NAME = "//h2"
+
+# TABS
+POSTS_TAB_ACTIVE_XPATH = "//*[@href='/profile/{username}' and @class='profile-nav-link nav-item nav-link active']"
+FOLLOWERS_TAB_XPATH = "//*[@href='/profile/{username}/followers' and @class='profile-nav-link nav-item nav-link ']"
+FOLLOWING_TAB_XPATH = "//*[@href='/profile/{username}/following' and @class='profile-nav-link nav-item nav-link ']"
