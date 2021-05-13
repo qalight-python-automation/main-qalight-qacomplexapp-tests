94a49aad1386592e3a9f98fbbfeb350e9ab14705
wing <denkondrat13+1@gmail.com>
2021-05-13 21:29:44 +0300

    Lesson Log 13.05

---
+++ constants/chat.py
@@ -0,0 +1,4 @@
+"""Stores useful constants and link for Chat"""
+
+CHAT_INPUT_FIELD_XPATH = "//input[@id='chatField']"
+SELF_CHAT_MESSAGES_XPATH = "//div[@class='chat-self']"
--- constants/header.py
+++ constants/header.py
@@ -3,3 +3,4 @@
 SIGN_OUT_BUTTON_XPATH = '//button[@class="btn btn-sm btn-secondary"]'
 PROFILE_BUTTON_XPATH = "//*[@href='/profile/{username}'and @class='mr-2']/img[@data-original-title='My Profile']"
 CREATE_POST_BUTTON_XPATH = "//*[@href='/create-post']"
+CHAT_BUTTON_XPATH = "//*[@data-icon='comment']"
